1003fb03f7c563382d4922b2e87dbfc74e1f747c
Darrell Ng <darrell.ng.wn@gmail.com>
2025-10-21 09:11:28 +0800

    Initial Commit

---
+++ WorkoutTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WorkoutTracker
+//
+//  Created by Darrell Ng on 21/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WorkoutTracker/WorkoutTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutTrackerApp.swift
+//  WorkoutTracker
+//
+//  Created by Darrell Ng on 21/10/25.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
